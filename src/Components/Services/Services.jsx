1ae4a00b8414fc528af0a3c1f9dfe8f16476d81e
sajib689 <jibonraja11@gmail.com>
2024-04-08 21:54:57 +0600

    error get on route

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -10,7 +10,7 @@ const Services = ({ data }) => {
         <h1 data-aos="zoom-in-right" className="animate__animated animate__bounce text-5xl font-bold text-center">Our Services</h1>
         <hr className="md:w-[250px] mx-auto" />
       </div>
-      <div  className="mt-[80px] grid grid-cols-3 lg:grid-cols-3 md:grid-cols-3 gap-6">
+      <div  className="mt-[80px] grid grid-cols lg:grid-cols-3 md:grid-cols-3 gap-6">
         {data.map((service) => (
           <ServiceCard key={service.id} service={service}></ServiceCard>
         ))}
